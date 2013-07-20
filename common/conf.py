73908a96d5efec3526b50c3d895d1bb0a880f285
Volker Braun <vbraun.name@gmail.com>
2013-07-20 01:35:52 -0400

    initial commit

---
+++ common/conf.py
@@ -0,0 +1,40 @@
+import os, sys
+
+extensions = ['sphinx.ext.inheritance_diagram', 'sphinx.ext.todo',
+              'sphinx.ext.extlinks', 'sphinx.ext.mathjax']
+templates_path = [os.path.join(os.getcwd(), '..', 'common', 'themes'), 'templates']
+# The suffix of source filenames.
+source_suffix = '.rst'
+# The master toctree document.
+master_doc = 'index'
+# General information about the project.
+project = u""
+copyright = u'2005--2011, The Sage Development Team'
+release = '6.0'
+default_role = 'math'
+pygments_style = 'sphinx'
+
+pythonversion = sys.version.split(' ')[0]
+# Python and Sage trac ticket shortcuts. For example, :trac:`7549` .
+
+# Sage trac ticket shortcuts. For example, :trac:`7549` .
+extlinks = {
+    'python': ('http://docs.python.org/release/'+pythonversion+'/%s', ''),
+    'trac': ('http://trac.sagemath.org/%s', 'trac ticket #'),
+    'wikipedia': ('http://en.wikipedia.org/wiki/%s', 'Wikipedia article '),
+    'arxiv': ('http://arxiv.org/abs/%s', 'Arxiv '),
+    'oeis': ('http://oeis.org/%s', 'OEIS sequence '),
+    'doi': ('http://dx.doi.org/%s', 'doi:'),
+    'mathscinet': ('http://www.ams.org/mathscinet-getitem?mr=%s', 'MathSciNet ')
+    }
+html_theme = 'sage'
+
+# Theme options are theme-specific and customize the look and feel of
+# a theme further.  For a list of options available for each theme,
+# see the documentation.
+html_theme_options = {}
+html_favicon = 'favicon.ico'
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [os.path.join(os.getcwd(), '..', 'common', 'themes')]
+
